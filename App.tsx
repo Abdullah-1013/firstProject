7d9edfd62bf74b969e934cdb1ca78d8507b04e4a
Abdullah-1013 <sp22-bcs-136@cuilahore.edu.pk>
2024-07-08 12:03:10 +0500

    RestFul API data fetching

--- App.tsx
+++ App.tsx
@@ -1,37 +1,29 @@
-import React from "react";
-import { NavigationContainer} from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { Button, Text, View } from "react-native";
-const tabs=createMaterialTopTabNavigator();
+import React,{useEffect,useState}from "react";
+import { Text,View } from "react-native";
 const App=()=>{
+  const [data,setdata]=useState(undefined);
+  const getAPI= async()=>{
+    const url="https://jsonplaceholder.typicode.com/posts/1";
+    let result=await fetch(url);
+    result=await result.json();
+    setdata(result);
+  }
+  useEffect(()=>{
+    getAPI();
+  },[])
+
   return(
-    <NavigationContainer>
-     <tabs.Navigator>
-      <tabs.Screen name="Home" component={Home}></tabs.Screen>
-
-      <tabs.Screen name="Login" component={Login}></tabs.Screen>
-
-
-     </tabs.Navigator>
-      
-      
-    </NavigationContainer>
+    <View>
+      <Text style={{fontSize:30,fontWeight:"bold"}}>RESTFUL API</Text>
+      {
+        data ?<View>
+          <Text style={{fontSize:30, fontWeight:"bold"}}>Id: {data.id}</Text>
+          <Text style={{fontSize:30, fontWeight:"bold"}}>userId: {data.userId}</Text>
+          <Text style={{fontSize:30, fontWeight:"bold"}}>Title: {data.title}</Text>
+          <Text style={{fontSize:30, fontWeight:"bold"}}>Body: {data.body}</Text>
+        </View>:null
+      }
+    </View>
   );
-    
 };
-const Home=()=>{
-  return(
-    <View>
-      <Text>Hello</Text>
-    </View>
-  )
-}
-const Login=(props)=>{
-  return(
-    <View>
-      <Text>Bye</Text>
-      <Button title=" Go To Login Page" onPress={()=>props.navigation.navigate("Home")}></Button>
-    </View>
-  )
-}
 export default App;
