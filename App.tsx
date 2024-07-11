c5df638ca9965f2575857c7f2c27b04ceb4cd685
Abdullah-1013 <sp22-bcs-136@cuilahore.edu.pk>
2024-07-11 11:53:46 +0500

    POST API Using Form and Validation

--- App.tsx
+++ App.tsx
@@ -1,29 +1,46 @@
-import React,{useEffect,useState}from "react";
-import { Text,View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, TextInput, View } from "react-native";
 const App=()=>{
-  const [data,setdata]=useState(undefined);
-  const getAPI= async()=>{
-    const url="https://jsonplaceholder.typicode.com/posts/1";
-    let result=await fetch(url);
-    result=await result.json();
-    setdata(result);
-  }
-  useEffect(()=>{
-    getAPI();
-  },[])
+    const[name,setname]=useState('');
+    const[age,setage]=useState('');
+    const[email,setemail]=useState('');
 
-  return(
-    <View>
-      <Text style={{fontSize:30,fontWeight:"bold"}}>RESTFUL API</Text>
-      {
-        data ?<View>
-          <Text style={{fontSize:30, fontWeight:"bold"}}>Id: {data.id}</Text>
-          <Text style={{fontSize:30, fontWeight:"bold"}}>userId: {data.userId}</Text>
-          <Text style={{fontSize:30, fontWeight:"bold"}}>Title: {data.title}</Text>
-          <Text style={{fontSize:30, fontWeight:"bold"}}>Body: {data.body}</Text>
-        </View>:null
-      }
-    </View>
-  );
-};
+    const savedata=async()=>{
+        if(!name||!age||!email)
+        {
+            Alert.alert("Please fill all the fields");
+            return;
+        }
+        const url="http://10.0.2.2:3000/User";
+        let result=fetch(url,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+
+            },
+            body:JSON.stringify({name,age,email})
+
+        });
+        result=(await result).json();
+        if(result){
+            console.warn("Data is saved successfully");
+        }
+
+    }
+    return(
+        <View>
+            <TextInput value={name} onChangeText={(text)=>setname(text)} placeholder="Enter the name"></TextInput>
+            <TextInput value={age} onChangeText={(text)=>setage(text)} placeholder="Enter the age"></TextInput>
+            <TextInput value={email} onChangeText={(text)=>setemail(text)} placeholder="Enter the email"></TextInput>
+            <Button title="Save Data" onPress={savedata}></Button>
+
+        </View>
+
+
+
+
+    );
+
+
+}
 export default App;
