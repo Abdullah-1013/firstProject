f37a3ce05f6cfebc0b42391210cac97eb47a783a
Abdullah-1013 <sp22-bcs-136@cuilahore.edu.pk>
2024-07-13 14:25:43 +0500

    List

--- App.tsx
+++ App.tsx
@@ -1,50 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Button, Text, View } from "react-native";
 const App=()=>{
-    const[Data,setData]=useState([])
+    const[data,Setdata]=useState([]);
     const getAPI=async()=>{
-        const url="http://10.0.2.2:3000/User";
+        const url='http://10.0.2.2:3000/User';
         let result=await fetch(url);
         result=await result.json();
-        console.warn(result);
         if(result){
-            setData(result)
+            Setdata(result);
+            
         }
-      }
-    const deleteData=async(id)=>{
-        const url="http://10.0.2.2:3000/User";
-        let result=await fetch(`${url}/${id}`,{
-            method:"delete"
-        });
-        if(result){
-            console.warn("User deleted Successfully");
-            getAPI();
-        }
+
+
     }
+
     useEffect(()=>{
         getAPI();
     },[])
     return(
         <View>
             {
-                Data.length ?
-                Data.map((item)=>
-                <View>
-                    <Text style={{fontSize:30, color:"red", fontWeight:"bold"}}>{item.name}</Text>
-                    <Text style={{fontSize:30, color:"green", fontWeight:"bold"}}>{item.age}</Text>
-                    <Text style={{fontSize:30, color:"pink", fontWeight:"bold"}}>{item.email}</Text>
-                    <Button title="Delete" onPress={()=>deleteData(item.id)}></Button>
+                data.length?
+                data.map((item)=><View>
+                    <View style><Text style={{fontSize:30}}>{item.age}</Text></View>
+                    <View><Button title="Delete"></Button></View>
 
-                </View>
-                )
-                :null
+                </View>)
+                :null                    
+        
+                
             }
         </View>
-
-
-
-
-    );
-
+    )
 }
 export default App;
